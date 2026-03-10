711321668c56a8b57894b04efc8b912b40d1e228
Ankit Kumar <128568501+kmarankit@users.noreply.github.com>
2026-03-10 16:47:57 +0000

    fix2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set to '/' for Netlify
+  base: './', // Ensure this is set to '/' for Netlify
 })
