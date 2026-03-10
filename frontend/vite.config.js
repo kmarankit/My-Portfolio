3db9d81c23faef621318107c957f1b6cb0e7fe39
Ankit Kumar <128568501+kmarankit@users.noreply.github.com>
2026-03-10 16:45:12 +0000

    fix1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',// Isse '0.0.0.0' enable ho jata hai (All network interfaces)
-    port: 5173,      // Aapka default port
-    strictPort: true // Agar port busy ho toh ye fail ho jaye, doosre port par na jaye
-  }
+  base: '/', // Ensure this is set to '/' for Netlify
 })
